1530c974c805b35f7b4b952f1439d21b526bd8ca
Wachirawit_Best <109467917+BestAsora@users.noreply.github.com>
2023-09-06 10:22:56 +0700

    Update 2.2-Rock Scissors Paper Game.r

--- DataRockie Bootcamp 7/2.2-Rock Scissors Paper Game.r
+++ DataRockie Bootcamp 7/2.2-Rock Scissors Paper Game.r
@@ -1,58 +1,59 @@
-junken <- function() {
-  print("Welcome to Rock Scissors Paper Game!")
-  print("Enter your username")
-  user_name <- readLines("stdin", 1)
-  print("This is your options")
-  options <- c("rock", "scissors", "paper")
-  print(paste(options, collapse = ", "))
+library(glue)
+
+game <- function() {
+  print("Welcome to Rock Scissors Paper Game")
+  username <- readline("Please, Enter your username: ")
+  print("Game Start!!!")
   
-  count <- 0
-  user_wins <- 0
-  computer_wins <- 0
+  round <- 0
+  user_win <- 0
+  user_tie <- 0
+  user_lose <- 0
   
   while (TRUE) {
-    print("Choose one (type 'done' to exit): ")
-    user_select <- readLines("stdin", 1)
+    print("This is your choices. >> Rock/Scissors/Paper")
+    print("When you want to end the game. Type 'Finish'")
+    user_select <- readline("Select your choices: ")
+    options <- c("rock", "scissors", "paper")
     
-    if (tolower(user_select) == "done") {
-      print("Game over. Here are the results:")
-      cat("Total Round: ", count, "\n")
-      cat("User Wins: ", user_wins, "\n")
-      cat("Computer Wins: ", computer_wins, "\n")
-      if (user_wins > computer_wins) {
-        print(paste("Congratulation!", user_name, ", You win the game."))
-      } else if (computer_wins > user_wins) {
-        print("You should bring more luck. :)")
-      } else if (user_wins == computer_wins) {
-        print("How can this be!")
+    computer_select <- sample(options, 1)
+    
+    if(tolower(user_select) == "finish") {
+      print("This game has ended")
+      cat("Result", "\n")
+      print(glue("Total Round: {round}"))
+      print(glue("Win: {user_win}"))
+      print(glue("Draw: {user_tie}"))
+      print(glue("Lose: {user_lose}"))
+      if (user_win > user_lose) {
+        print(glue("Congratulation! {username} win this game!"))
+      } else if (user_win == user_lose) {
+        print("It's regrettable, just a bit more.")
+      } else {
+        print("Don't be sad. It's only a bad luck.")
       }
       break
     }
     
     if (!tolower(user_select) %in% options) {
-      print("Please choose from 'rock', 'scissors', or 'paper' only.")
+      print("Please select again")
       next
     }
     
-    computer_select <- sample(options, 1)
-    
-    if (user_select == computer_select) {
-      print("It's a tie!")
-      count <- count + 1
-    } else if (
-      (user_select == "rock" && computer_select == "scissors") ||
-      (user_select == "scissors" && computer_select == "paper") ||
-      (user_select == "paper" && computer_select == "rock")
-    ) {
-      print("You win!")
-      user_wins <- user_wins + 1
-      count <- count + 1
+    if (tolower(user_select) == computer_select) {
+      print("Tie")
+      round <- round + 1
+      user_tie <- user_tie + 1
+    } else if ((tolower(user_select) == "rock" && computer_select == "scissors") ||
+               (tolower(user_select) == "scissors" && computer_select == "paper") ||
+               (tolower(user_select) == "paper" && computer_select == "rock")) {
+      print("Win")
+      round <- round + 1
+      user_win <- user_win + 1
     } else {
-      print("You lose!")
-      computer_wins <- computer_wins + 1
-      count <- count + 1
+      print("Lose")
+      round <- round + 1
+      user_lose <- user_lose + 1
     }
   }
 }
-
-junken()
