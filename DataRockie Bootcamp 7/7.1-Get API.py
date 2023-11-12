2408ef51ab72810e29a35ca3ce67b6940c319e17
Wachirawit_Best <109467917+BestAsora@users.noreply.github.com>
2023-11-12 15:02:51 +0700

    Update 7.1-Get API.py

--- DataRockie Bootcamp 7/7.1-Get API.py
+++ DataRockie Bootcamp 7/7.1-Get API.py
@@ -1,1 +1,12 @@
+import requests
+import pandas as pd
 
+url = "http://universities.hipolabs.com/search?country=United States"
+
+result = requests.get(url)
+
+data_json = result.json()
+
+df = pd.DataFrame(data_json)
+
+df.to_csv("university_in_united_states")
